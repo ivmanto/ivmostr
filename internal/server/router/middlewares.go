76a4a572170772c110386bbb8d6cdb9ffd5af008
Nikolay Tonev <nikolay.tonev@gmail.com>
2023-12-22 22:21:06 +0100

    v0.0.3-dev final changes v0.0.3-allow host

--- internal/server/router/middlewares.go
+++ internal/server/router/middlewares.go
@@ -19,7 +19,7 @@ func accessControl(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, X-TOKEN-TYPE, X-GRANT-TYPE, X-IVM-CLIENT")
+		w.Header().Set("Access-Control-Allow-Headers", "Host, Origin, Content-Type, Authorization, X-TOKEN-TYPE, X-GRANT-TYPE, X-IVM-CLIENT")
 
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Max-Age", "3600")
