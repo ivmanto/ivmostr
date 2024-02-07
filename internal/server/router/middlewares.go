609f7b60422b581818003be1c8af4d00289145a9
Nikolay Tonev <nikolay.tonev@gmail.com>
2024-02-07 09:04:56 +0100

    vv0.0.20-dev fix some wl bl-5

--- internal/server/router/middlewares.go
+++ internal/server/router/middlewares.go
@@ -47,7 +47,7 @@ func healthcheck(h http.Handler) http.Handler {
 }
 
 // Handles the rate Limit control
-func rateLimiter(lists nostr.ListRepo, wlst, blst []string) func(next http.Handler) http.Handler {
+func rateLimiter(lists nostr.ListRepo, wlst, blst []string) func(h http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
